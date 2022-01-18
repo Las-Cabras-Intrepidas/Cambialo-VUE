cd7a714ff7a7e86f18c8795ec3c0d6b1428d3582
Soleny Yumare <solenyyumare@gmail.com>
2022-01-18 10:55:09 +0000

    Products with grid already working

---
+++ src/components/ProductsComponents/ProductsCard.vue
@@ -0,0 +1,70 @@
+<template>
+<h4>Our Products</h4>
+  <div class="container">
+    <div class="row-container">
+    <div class="row" v-for="product in products" :key="product.id">
+      <img :src="product.picture" :alt="product.title" />
+      <h4>{{ product.title }}</h4>
+      <p class="availability">{{ product.available ? "Disponible" :  "No Disponible"}}</p>
+      <p>{{ product.description }}</p>
+    </div>
+  </div>
+  </div>
+</template>
+
+<script>
+import products from '../../assets/data/producto.json'
+
+export default {
+  name: 'ProductsCard',
+  data () {
+    return {
+      products: products
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+.container {
+  display: flex;
+  height: 100%;
+  width: 100%;
+}
+
+.row {
+  border: 2px solid grey;
+  border-radius: 8px;
+  box-shadow: 1px 1px 1px grey;
+}
+
+.row-container {
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  grid-template-rows: 1fr;
+  grid-column-gap: 8px;
+  grid-row-gap: 8px;
+}
+
+img {
+  width: 200px;
+}
+
+.availability {
+  color: green;
+  font-weight: 600;
+}
+
+p {
+  font-size: 15px;
+  padding: 5px;
+}
+
+/* mediaqueries */
+@media (min-width: 350px) and (max-width: 600px) {
+  .row-container {
+      grid-template-columns: repeat(2, 1fr);
+  }
+}
+</style>
--- src/views/Products.vue
+++ src/views/Products.vue
@@ -1,40 +1,14 @@
 <template>
-  <!-- <div id="technology" class="column is-half-touch is-one-quarter-desktop">
-    <div class="product-card">
-        <img src="img/products/tecnologia-movil.webp" alt="" />
-        <div class="text-product">
-            <p class="green">Disponible</p>
-            <p class="has-text-weight-bold">iPhone 11</p>
-            <p class="has-text-left">
-                iPhone 11 de 64gb negro tiene 3 fundas, con su factura y garantía, está nuevo pero me gusta más Android.
-            </p>
-        </div>
-    </div>
-  </div> -->
-  <!-- <div v-for="product in products" :key="product.id"> Producto {{data}}</div> -->
-    <div class="container">
-      <div class="row">
-        <h4>Our Products</h4>
-      </div>
-      <div class="row" v-for="product in info" :key="product.id">
-        <p scope="row"> {{ product.id }}</p>
-          <img :src="product.picture" :alt="product.title" />
-          <p>{{ product.title }}</p>
-          <p>{{ product.availability }}</p>
-          <p>{{ product.description }}</p>
-      </div>
-    </div>
+  <ProductsCard />
 </template>
 
 <script>
-import products from '../assets/data/producto.json'
+import ProductsCard from '../components/ProductsComponents/ProductsCard.vue'
 
 export default {
   name: 'Products',
-  data () {
-    return {
-      products: products
-    }
+  components: {
+    ProductsCard
   }
 }
 </script>
