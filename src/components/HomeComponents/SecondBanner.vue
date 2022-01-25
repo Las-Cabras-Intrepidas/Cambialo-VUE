1fdb88d3cde42405ac7248c123f07f16ec993da3
abuenososa <mac@MacBook-Pro.lan>
2022-01-25 17:16:28 +0100

    Añadido color en transparencia al 2º banner

--- src/components/HomeComponents/SecondBanner.vue
+++ src/components/HomeComponents/SecondBanner.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="banner-trading">
+    <div class="color-banner"></div>
     <img
       class="img-banner-product"
       src="../../assets/img/Trading2.jpg"
@@ -21,12 +22,17 @@ export default {
   justify-content: center;
   align-items: center;
 }
-
+.color-banner {
+  background-color: #012bb379;
+  width: 100%;
+  height: 65%;
+  position: absolute;
+}
 .img-banner-product {
   height: 450px;
   width: 100%;
-  margin: 2rem 0;
-  opacity: 0.8;
+  margin: 3rem 0;
+  background-size: cover;
 }
 
 .img-slogan {
