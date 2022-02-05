4cc8bfb8abb98fb9da56ec8d7e22bbd9baa03bc2
Soleny Yumare <solenyyumare@gmail.com>
2022-02-05 11:33:29 +0000

    Code of odal

--- src/components/ProductsComponents/ProductDetail.vue
+++ src/components/ProductsComponents/ProductDetail.vue
@@ -10,48 +10,45 @@
       <button @click="showModal = true" class="button">Cambiar</button>
     </div>
   </div>
-  <div class="modal-container">
-    <transition name="fade" appear>
-      <div class="modal-overlay" v-if="showModal" @click="showModal = false"></div>
-    </transition>
-    <transition name="pop" appear>
-      <div class="modal" role="dialog" v-if="showModal">
-        <span @click="showModal = false" class="button-close">
-          <font-awesome-icon class="icon" icon="times-circle" />
-        </span>
-        <form id="contact">
-          <h2>Rellena el formulario para contactar</h2>
-          <fieldset>
-            <input placeholder="Nombre" type="text" />
-          </fieldset>
-          <fieldset>
-            <input placeholder="usuario@email.com" type="email" />
-          </fieldset>
-          <fieldset>
-            <input placeholder="Tu teléfono (opcional)" type="tel" />
-          </fieldset>
-          <fieldset>
-            <input placeholder="¿Qué ofreces?" type="text" />
-          </fieldset>
-          <fieldset>
-            <textarea placeholder="¿Necesitas dar más información?"></textarea>
-          </fieldset>
-          <button @click="showMessage = true" class="button">Enviar</button>
-        </form>
-      </div>
-    </transition>
-  </div>
-  <!--  invento -->
-  <div class="modal-container-message">
-    <transition name="fade" appear>
-      <div class="modal-overlay-message" v-if="showMessage" @click="showMessage = false"></div>
-    </transition>
-    <transition name="pop" appear>
-      <div class="modal-message" role="dialog" v-if="showMessage">
-        <div class="msg">¡ENVIADO!</div>
-      </div>
-    </transition>
-  </div>
+  <!-- Modal -->
+  <transition name="fade" appear>
+    <div class="modal-overlay flex" v-if="showModal">
+      <transition name="pop" appear>
+        <div class="modal" role="dialog" v-if="showModal">
+          <span @click="showModal = false" class="button-close">
+            <font-awesome-icon class="icon" icon="times-circle" />
+          </span>
+          <form id="contact">
+            <h2>Rellena el formulario para contactar</h2>
+            <fieldset>
+              <input placeholder="Nombre" type="text" />
+            </fieldset>
+            <fieldset>
+              <input placeholder="usuario@email.com" type="email" />
+            </fieldset>
+            <fieldset>
+              <input placeholder="Tu teléfono (opcional)" type="tel" />
+            </fieldset>
+            <fieldset>
+              <input placeholder="¿Qué ofreces?" type="text" />
+            </fieldset>
+            <fieldset>
+              <textarea placeholder="¿Necesitas dar más información?"></textarea>
+            </fieldset>
+            <button @click="showMessage = true" class="button">Enviar</button>
+          </form>
+        </div>
+      </transition>
+      <!--  invento -->
+      <transition name="fade" appear v-if="showMessage" @click="showMessage = false">
+        <transition name="pop" appear>
+          <div class="modal-send flex" role="dialog" v-if="showMessage">
+            <p class="msg">¡ENVIADO!</p>
+          </div>
+        </transition>
+      </transition>
+    </div>
+  </transition>
 </template>
 
 <script>
@@ -137,44 +134,52 @@ span {
   margin-top: 0.75rem;
 }
 
-.modal {
-  position: absolute;
-  position: fixed;
-  right: 10%;
+.flex {
+  display: flex;
   justify-content: center;
-  top: 5%;
+  align-items: center;
+}
+.modal-overlay {
+  position: absolute;
+  top: 70px;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  background: #2a3d5094;
+  height: 100vh;
+}
+
+@media (max-width: 900px) {
+  .modal-overlay {
+    position: absolute;
+    top: 135px;
+  }
+}
+.modal {
   text-align: center;
   border-radius: 15px;
   box-shadow: 0 15px 10px rgba(0, 0, 0, 0.24);
   background: #fff;
-  z-index: 999;
   transform: none;
   box-sizing: border-box;
-  -webkit-box-sizing: border-box;
-  -moz-box-sizing: border-box;
-  -webkit-font-smoothing: antialiased;
-  -moz-font-smoothing: antialiased;
-  -o-font-smoothing: antialiased;
-  /* font-smoothing: antialiased; */
   text-rendering: optimizeLegibility;
   min-width: 30%;
   max-width: 80%;
 }
-
-.modal-overlay {
-  content: "";
+.modal-send {
   position: absolute;
-  position: fixed;
-  justify-content: center;
-  text-align: center;
-  top: 0;
-  right: 0;
-  bottom: 0;
-  left: 0;
-  z-index: 998;
-  background: #2a3d50;
-  opacity: 0.65;
+  background: rgba(144, 238, 144, 0.616);
+  border: 1px solid green;
+  border-radius: 8px;
+  top: 500px;
+  width: 30%;
 }
+
+.msg {
+  font-size: 18px;
+  color: #000;
+}
+
 #contact input[type="text"],
 #contact input[type="email"],
 #contact input[type="tel"],
@@ -202,10 +207,10 @@ span {
     background-color: #000292;
   }
   h2 {
-    display: block;
     font-size: 1.7em;
     font-weight: 600;
     margin-bottom: 35px;
+    margin-top: 0;
   }
   input {
     text-indent: 8px;
@@ -262,7 +267,7 @@ fieldset {
 }
 
 /* ---------------------------------- */
-.fade-enter-active,
+/* .fade-enter-active,
 .fade-leave-active {
   transition: opacity 0.4s linear;
 }
@@ -281,106 +286,6 @@ fieldset {
 .pop-leave-to {
   opacity: 0;
   transform: scale(0.3) translateY(-50%);
-}
-
-@media (min-width: 500px) {
-  .modal {
-    max-width: 70%;
-    top: 15%;
-    right: 15%;
-  }
-}
-
-@media (min-width: 700px) {
-  .modal {
-    max-width: 55%;
-    top: 15%;
-    right: 22.5%;
-  }
-}
-
-@media (min-width: 950px) {
-  .modal {
-    max-width: 50%;
-    top: 15%;
-    right: 30%;
-  }
-}
-
-/* invento */
-
-.modal-message {
-  position: absolute;
-  position: fixed;
-  right: 35%;
-  justify-content: center;
-  top: 45%;
-  text-align: center;
-  border-radius: 15px;
-  box-shadow: 0 15px 10px rgba(0, 0, 0, 0.24);
-  background: lightgreen;
-  z-index: 999;
-  transform: none;
-  box-sizing: border-box;
-  -webkit-box-sizing: border-box;
-  -moz-box-sizing: border-box;
-  -webkit-font-smoothing: antialiased;
-  -moz-font-smoothing: antialiased;
-  -o-font-smoothing: antialiased;
-  /* font-smoothing: antialiased; */
-  text-rendering: optimizeLegibility;
-  min-height: 5%;
-  min-width: 20%;
-  max-width: 60%;
-}
-
-.modal-overlay-message {
-  content: "";
-  position: absolute;
-  position: fixed;
-  justify-content: center;
-  text-align: center;
-  top: 0;
-  right: 0;
-  bottom: 0;
-  left: 0;
-  z-index: 998;
-  background: #2a3d50;
-  opacity: 0.65;
-}
-
-.msg {
-  margin: 10 auto;
-  align-self: center;
-  font-weight: 600;
-  color: rgb(2, 107, 2);
-  font-size: 18px;
-}
-
-@media (min-width: 950px) {
-  .msg {
-    margin: 10px auto;
-  }
-  .modal-message {
-    right: 40%;
-    width: 30%;
-  }
-}
-
-@media (min-width: 400px) {
-  .msg {
-    margin: 10px auto;
-  }
-  .modal-message {
-    right: 30%;
-    width: 40%;
-  }
-}
-
-@media (min-width: 1250px) {
-  .modal-message {
-    right: 40%;
-    width: 20%;
-  }
-}
+} */
+/* mediaqueries */
 </style>
