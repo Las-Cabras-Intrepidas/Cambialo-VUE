49d9dc6fd0ed611de8e77d526966ff3f605b1018
Barbara Hernandez <b.hernandez7hp@gmail.com>
2022-02-09 19:29:00 +0100

    stable commit

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,17 @@
 module.exports = {
-  overrides: [
-    {
-      files: [
-        '**/__tests__/*.{j,t}s?(x)',
-        '**/tests/unit/**/*.spec.{j,t}s?(x)'
-      ],
-      env: {
-        jest: true
-      }
-    }
-  ]
+  root: true,
+  env: {
+    node: true
+  },
+  extends: [
+    'plugin:vue/vue3-essential',
+    '@vue/standard'
+  ],
+  parserOptions: {
+    parser: 'babel-eslint'
+  },
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+  }
 }
--- src/components/ProductsComponents/ProductsCard.test.js
+++
@@ -1,8 +0,0 @@
-// import { mount } from '@vue/test-utils'
-// import productSearch from './ProductsCard.vue'
-
-// describe('Product Search', () => {
-//   test('Comprobar input de barra de búsqueda', () => {
-//     const wrapper = mount(productSearch, {})
-//   })
-// })
