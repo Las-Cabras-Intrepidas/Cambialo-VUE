875716293a4759c60a64d752216d1378f863d6d4
Barbara Hernandez <b.hernandez7hp@gmail.com>
2022-02-09 18:58:33 +0100

    delete test

--- src/components/ProductsComponents/ProductsCard.test.js
+++ src/components/ProductsComponents/ProductsCard.test.js
@@ -1,11 +1,8 @@
-import { mount } from '@vue/test-utils'
-import productSearch from './ProductsCard.vue'
-// import BannerComponent from './BannerComponent.vue'
+// import { mount } from '@vue/test-utils'
+// import productSearch from './ProductsCard.vue'
 
-describe('Product Search', () => {
-  test('Comprobar input de barra de búsqueda', () => {
-    const wrapper = mount(productSearch, {})
-    // await wrapper.find('#search').setValue('balon')
-    // expect(wrapper.find('#productTitle').text()).toBeEqual('Balón')
-  })
-})
+// describe('Product Search', () => {
+//   test('Comprobar input de barra de búsqueda', () => {
+//     const wrapper = mount(productSearch, {})
+//   })
+// })
